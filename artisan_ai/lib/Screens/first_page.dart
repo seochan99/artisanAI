9aaf804ae7927d97ba9ed9904f82251568cf1d02
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-04-10 09:50:57 +0900

    style : first_page 테마 색상 변경

--- artisan_ai/lib/Screens/first_page.dart
+++ artisan_ai/lib/Screens/first_page.dart
@@ -23,36 +23,47 @@ class _FirstPageState extends State<FirstPage> {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          // text입력창
-          TextField(
-            controller: _controller,
-            // 데코레이션
-            decoration: const InputDecoration(
-              hintText: "Please enter your prompt",
-              icon: Icon(Icons.send),
-              counterText: '0 characters',
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black, width: 2),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            // text입력창
+            TextField(
+              controller: _controller,
+              // 데코레이션
+              maxLength: 30,
+              decoration: const InputDecoration(
+                  hintText: "Please enter your prompt",
+                  icon: Icon(Icons.send, color: Colors.black),
+                  filled: true,
+                  // 인풋 클릭 전 테두리 색상
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  // 인풋 클릭시 테두리 색상
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  )),
             ),
-          ),
 
-          // text입력창에 입력된 text를 result_page.dart로 전달
-          TextButton(
-              onPressed: () {
-                String prompt = _controller.text;
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => ResultPage(prompt: prompt)),
-                );
-              },
-              child: const Text("Get Results"))
-        ],
+            // text입력창에 입력된 text를 result_page.dart로 전달
+            TextButton(
+                onPressed: () {
+                  String prompt = _controller.text;
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => ResultPage(prompt: prompt)),
+                  );
+                },
+                child: const Text(
+                  "Get Results",
+                  style: TextStyle(color: Colors.black),
+                ))
+          ],
+        ),
       ),
     );
   }
